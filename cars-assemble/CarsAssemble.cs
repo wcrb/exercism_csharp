26aaf56d37bdc5f6561c08e4d2bc5c2af1791a15
wcrb <mailAthonHack@protonmail.com>
2022-05-08 00:56:10 +0200

    implement logic, all tests passed

--- cars-assemble/CarsAssemble.cs
+++ cars-assemble/CarsAssemble.cs
@@ -2,18 +2,24 @@ using System;
 
 static class AssemblyLine
 {
+    const int maxNumberOfCarsProducedPerHour = 221;
     public static double SuccessRate(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
+        if (speed == 10) return 0.77;
+        else if (speed == 9) return 0.8;
+        else if (speed >= 5) return 0.9;
+        else if (speed >= 1) return 1;
+
+        return 0;
     }
     
     public static double ProductionRatePerHour(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+        return SuccessRate(speed) * speed * maxNumberOfCarsProducedPerHour;
     }
 
     public static int WorkingItemsPerMinute(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+        return (int)ProductionRatePerHour(speed) / 60;
     }
 }
