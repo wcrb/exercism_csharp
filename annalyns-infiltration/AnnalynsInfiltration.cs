4bb1e18d076610ebfa681c99d41ba43e1dd7906b
wcrb <mailAthonHack@protonmail.com>
2022-05-08 00:31:18 +0200

    implement logic, all tests passed

--- annalyns-infiltration/AnnalynsInfiltration.cs
+++ annalyns-infiltration/AnnalynsInfiltration.cs
@@ -4,21 +4,25 @@ static class QuestLogic
 {
     public static bool CanFastAttack(bool knightIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFastAttack() method");
+        return !knightIsAwake;
     }
 
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSpy() method");
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
     }
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSignalPrisoner() method");
+        return prisonerIsAwake && (!archerIsAwake);
     }
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
+        if(petDogIsPresent) return (!archerIsAwake);
+        else
+        {
+            return prisonerIsAwake && !(archerIsAwake || knightIsAwake);
+        }
     }
 }
